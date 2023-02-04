1441e9f98437903056ea4b97328b10705186cc68
kishannen <98503879+kishannen@users.noreply.github.com>
2023-02-04 23:12:30 +0800

    1 file modified

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PagenotfoundComponent implements OnInit {
   
   headColor = 'brown';
+  showItem = true;
   
   message = '';
   onButtonClick() {
