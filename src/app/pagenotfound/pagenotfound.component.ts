53c2dfddecd0e2cc566ed59d6441aea5c3f0d054
kishannen <98503879+kishannen@users.noreply.github.com>
2023-02-04 23:17:50 +0800

    2 files modified

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css'],
 })
 export class PagenotfoundComponent implements OnInit {
-  
   headColor = 'brown';
   showItem = true;
-  
+  elements = [
+    'Sorry, the content of this page is missing.',
+    'I am still constructing my website for better experience.',
+    'Thank you for your kind understanding',
+  ];
+
   message = '';
   onButtonClick() {
     this.message = 'kisses.reyes010@gmail.com';
